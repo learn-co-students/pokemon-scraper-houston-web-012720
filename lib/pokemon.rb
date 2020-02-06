a2f36f40cab16adb2a1db94d8d740fdcfe8f955a
github username <github email address>
2020-02-06 14:31:25 -0600

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
+require_relative "../bin/environment"
+require 'pry'
 class Pokemon
+
+    attr_reader :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        
+        db.execute("INSERT INTO pokemon(name, type) VALUES (\"#{name}\", \"#{type}\")")
+    end
+
+    def self.find(id, db)
+        array = db.execute("SELECT pokemon.id, pokemon.name, pokemon.type FROM pokemon WHERE id=#{id}")
+        hash = {:id => array[0][0], :name => array[0][1], :type => array[0][2], :db => db}
+
+        Pokemon.new(hash)
+    end
 end

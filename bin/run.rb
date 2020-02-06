14e7cc8856868115254325aa2e023b84457a660a
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-02-06 14:07:22 -0600

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,12 @@
 require_relative "environment"
+require 'pry'
 
-Scraper.new(@db).scrape
+scr = Scraper.new(@db).scrape
+
+
 
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
+
+pp all_pokemon
 
 # test out your code here!
